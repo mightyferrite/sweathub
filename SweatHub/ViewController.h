1a461ab5a35ca1005bbbbf609df98b3ac6cdab4e
John Reine <john@johnreine.com>
2015-08-21 00:54:02 -0400

    Initial Commit

---
+++ SweatHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SweatHub
+//
+//  Created by John Reine on 8/21/15.
+//  Copyright (c) 2015 John Reine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
